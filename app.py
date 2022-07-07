676eefe64150eb3b12267502a0a1bd7891c20584
TEMFACK DERICK <dericktemfack@gmail.com>
2022-07-07 14:21:21 +0100

    Add new model

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/translate', methods=["POST"])
 def translate():
     request_data = request.json
-    input_text = request_data['input']
+    input_text = request_data['input_text']
+    source_lang = request_data['source_lang']
+    dest_lang = request_data['dest_lang']
+    
+    return jsonify({"input_data":input_data,"source_lang":source_lang,"dest_lang":dest_lang})
+    
 
 if __name__=="__main__":
     app.run(port=8000, debug=True)
