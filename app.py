60ce8cf62829ece3dbe1a3645e45c64cf73f5bdf
TEMFACK DERICK <dericktemfack@gmail.com>
2022-07-07 17:29:38 +0100

    Refactoring code

--- app.py
+++ app.py
@@ -1,39 +1,38 @@
-import pickle as pk
-
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 
 import numpy as np
-from keras.models import load_model
 
 from utils import *
 
 app = Flask(__name__)
 
 
-@app.route('/translate', methods=["POST"])
+@app.route('/translate', methods=["POST","GET"])
 def translate():
-    request_data = request.json
-    input_text = np.array([request_data['input_text']])
-    source_lang = request_data['source_lang']
-    dest_lang = request_data['dest_lang']
+    if request.method == "POST":      
+        if request.is_json:
+            request_data = request.json
+            input_text = np.array([request_data['input_text']])
+            source_lang = request_data['source_lang']
+            dest_lang = request_data['dest_lang']
+            
+            translated = make_translation(source_lang, dest_lang, input_text)
+            return jsonify({"result":translated})
+        else:
+            input_text = np.array([request.form['input_text']])
+            source_lang = request.form['source_lang']
+            dest_lang = request.form['dest_lang']
+            
+            translated = make_translation(source_lang, dest_lang, input_text)
+
+            return render_template('index.html', translated=translated, input_text=request.form['input_text'])
+    elif request.method == "GET":
+        return render_template('index.html')
     
-    # Load model
-    model = load_model('models/model_v1.h5')
     
-    # load model parameters
-    model_parameters = pk.load(open("models/model_parameter_v1.pk","rb"))
-    
-    if source_lang=="en" and dest_lang=="deu" :
-        encode_input =encode_sequences(model_parameters['fren_tokenizer'], model_parameters["gho_length"], input_text)
-        translated = predict_sequence(model,model_parameters["gho_tokenizer"], encode_input)
-        return jsonify({"result":translated})
-    elif source_lang=="deu" and dest_lang=="en": 
-        encode_input = encode_sequences(model_parameters['gho_tokenizer'], model_parameters["gho_length"], input_text)
-        translated = predict_sequence(model,model_parameters["fren_tokenizer"], encode_input)
-        return jsonify({"result":translated})
-    else:
-        return jsonify({"result":"Error"})
-    
+@app.route("/", methods=["GET"])
+def home():
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(port=8000, debug=True)
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+import pickle as pk
+
 import numpy as np
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model
 
 def encode_sequences(tokenizer, length, lines):
     # integer encode sequences
@@ -26,3 +29,20 @@ def predict_sequence(model, tokenizer, source):
 			break
 		target.append(word)
 	return ' '.join(target)
+
+def make_translation(source_lang, dest_lang, input_text):
+      # Load model
+    model = load_model('models/model_v1.h5')
+    
+    # load model parameters
+    model_parameters = pk.load(open("models/model_parameter_v1.pk","rb"))
+    
+    if source_lang=="en" and dest_lang=="deu" :
+        encode_input =encode_sequences(model_parameters['fren_tokenizer'], model_parameters["gho_length"], input_text)
+        translated = predict_sequence(model,model_parameters["gho_tokenizer"], encode_input)
+        return translated
+    elif source_lang=="deu" and dest_lang=="en": 
+        encode_input = encode_sequences(model_parameters['gho_tokenizer'], model_parameters["gho_length"], input_text)
+        translated = predict_sequence(model,model_parameters["fren_tokenizer"], encode_input)
+        return translated
+    
